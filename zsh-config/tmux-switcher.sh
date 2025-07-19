d927963a6812a3cc8c5485156d47021b85573bcb
Faisal Budiono <ucul.project@gmail.com>
2025-07-19 18:40:47 +0700

    fix(script): handle no session chosen on tmux switcher

--- zsh-config/tmux-switcher.sh
+++ zsh-config/tmux-switcher.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 CHOOSEN=$(tmux ls | awk -F ":" '{print $1}' | sort -r | fzf)
 
+if [ -z "$CHOOSEN" ]; then
+    echo "No session chosen"
+    return
+fi
+
 tmux switchc -t $CHOOSEN
