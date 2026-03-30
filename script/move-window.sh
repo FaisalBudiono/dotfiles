30665082bf4679c8a6b1bf88dde7ea49fac066e7
Faisal Budiono <ucul.project@gmail.com>
2026-03-31 03:42:38 +0700

    fix(script): remove printing to dummy file

--- script/move-window.sh
+++ script/move-window.sh
@@ -1,19 +1,11 @@
 #!/bin/env bash
 
-file=/home/ucul/script/kuda.txt
-# echo "hoho" >> $file
-# pwd >> $file
-# echo $HOME >> $file
-
 workspaceNumber=$(rofi -dmenu -p "Move to workspace")
 
 # Abort if user cancels
 [ -z "$workspaceNumber" ] && exit
 
-echo $workspaceNumber >>$file
-
 if ! [[ "$workspaceNumber" =~ ^[0-9]+$ ]]; then
-    echo "Sorry integers only" >>$file
     exit
 fi
 
