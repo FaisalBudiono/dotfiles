6b2ff5508e17ac37afad8b6163773a42b61265b1
Faisal Budiono <ucul.project@gmail.com>
2026-08-13 14:59:18 +0700

    fix(nvim): fix comment nvim not working for certain filetypes
    Filetype like .env that doesn't have treesitter support will show
    "[Comment.nvim] nil" error whenever the comment command is executed.

--- .config/nvim/lua/FaisalBudiono/plugin/comment.lua
+++ .config/nvim/lua/FaisalBudiono/plugin/comment.lua
@@ -6,12 +6,27 @@ return {
         local comment = require("Comment")
 
         comment.setup({
-            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+            pre_hook = function(ctx)
+                -- If Treesitter/Comment.nvim fails to find a comment string for this filetype,
+                -- fallback safely to '#' for line comments and leave block comments alone.
+                local ok, _ = pcall(require, "Comment.utils")
+                if ok then
+                    -- Check if the current filetype lacks a valid internal config
+                    if vim.bo.filetype == "env" then
+                        return "#%s"
+                    end
+
+                    local filetypes = require("FaisalBudiono.filetype").filetypes
+                    if vim.bo.filetype == filetypes["eraser-diagram"] then
+                        return "//%s"
+                    end
+                end
+
+                local ts_context_cb =
+                    require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+
+                return ts_context_cb(ctx)
+            end,
         })
-
-        local ft = require("Comment.ft")
-        local filetypes = require("FaisalBudiono.filetype").filetypes
-
-        ft({ filetypes["eraser-diagram"] }, "//%s")
     end,
 }
