44b6084f531c31552067d38d6d5deb0f561ac6c0
Faisal Budiono <ucul.project@gmail.com>
2026-09-17 14:39:16 +0800

    feat(nvim): add treesitter for d2

--- .config/nvim/lua/FaisalBudiono/plugin/treesitter.lua
+++ .config/nvim/lua/FaisalBudiono/plugin/treesitter.lua
@@ -1,42 +1,50 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    dependencies = {},
-    config = function()
-        local ts = require("nvim-treesitter")
-        ts.setup({})
+    {
+        "ravsii/tree-sitter-d2",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        version = "*", -- use the latest git tag instead of main
+        build = "make nvim-install",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+        dependencies = {},
+        config = function()
+            local ts = require("nvim-treesitter")
+            ts.setup({})
 
-        ts.install({
-            "c",
-            "gitcommit",
-            "gitignore",
-            "go",
-            "gomod",
-            "gosum",
-            "javascript",
-            "jsdoc",
-            "json",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "php",
-            "phpdoc",
-            "query",
-            "rust",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-        })
+            ts.install({
+                "c",
+                "gitcommit",
+                "gitignore",
+                "go",
+                "gomod",
+                "gosum",
+                "javascript",
+                "jsdoc",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "php",
+                "phpdoc",
+                "query",
+                "rust",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+            })
 
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "*" },
-            callback = function(args)
-                pcall(vim.treesitter.start, args.buf)
-            end,
-        })
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = { "*" },
+                callback = function(args)
+                    pcall(vim.treesitter.start, args.buf)
+                end,
+            })
 
-        vim.treesitter.language.register("sql", { "sql", "mysql" })
-    end,
+            vim.treesitter.language.register("sql", { "sql", "mysql" })
+        end,
+    },
 }
